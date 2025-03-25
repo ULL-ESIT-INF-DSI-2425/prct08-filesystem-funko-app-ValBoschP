5c3bbb92315defeb941dd9349b2c12d416268bdb
ValBoschP <alu0101485287@ull.edu.es>
2025-03-25 00:58:46 +0000

    feat: add `funkoType` and `funkoGenre`

---
+++ src/enums/FunkoGenre.ts
@@ -0,0 +1,8 @@
+export enum funkoGenre {
+  ANIMATION = "Animation",
+  MOVIES = "Movies & TV",
+  VIDEOGAMES = "Video Games",
+  SPORTS = "Sports",
+  ANIME = "Anime",
+  MUSIC = "Music",
+}
---
+++ src/enums/FunkoType.ts
@@ -0,0 +1,6 @@
+export enum funkoType {
+  POP = "Pop!",
+  POP_RIDES = "Pop! Rides",
+  VYNIL_SODA = "Vinyl Soda",
+  VYNIL_GOLD = "Vinyl Gold",
+}
