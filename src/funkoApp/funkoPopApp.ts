bc3c6a742d2ce2fa0c77425ba62e52165383d8f0
ValBoschP <alu0101485287@ull.edu.es>
2025-03-25 15:04:28 +0000

    refactor: `funkoPopApp` for duplicate liens

--- src/funkoApp/funkoPopApp.ts
+++ src/funkoApp/funkoPopApp.ts
@@ -5,203 +5,116 @@ import { funkoGenre } from "./enums/FunkoGenre.js";
 import { funkoType } from "./enums/FunkoType.js";
 import { Funko } from "./models/FunkoPop.js";
 
+const commonFunkoOptions = {
+  username: {
+    description: "Username",
+    alias: "u",
+    type: "string" as const,
+    demandOption: true,
+  },
+  id: {
+    description: "Funko ID",
+    alias: "i",
+    type: "string" as const,
+    demandOption: true,
+  },
+  name: {
+    description: "Funko Name",
+    alias: "n",
+    type: "string" as const,
+    demandOption: true,
+  },
+  description: {
+    description: "Funko Description",
+    alias: "d",
+    type: "string" as const,
+    demandOption: true,
+  },
+  type: {
+    description: "Funko Type",
+    alias: "t",
+    type: "string" as const,
+    choices: Object.values(funkoType),
+    demandOption: true,
+  },
+  genre: {
+    description: "Funko Genre",
+    alias: "g",
+    type: "string" as const,
+    choices: Object.values(funkoGenre),
+    demandOption: true,
+  },
+  franchise: {
+    description: "Funko Franchise",
+    alias: "f",
+    type: "string" as const,
+    demandOption: true,
+  },
+  number: {
+    description: "Funko Number",
+    alias: "num",
+    type: "number" as const,
+    demandOption: true,
+  },
+  exclusive: {
+    description: "Funko Exclusive",
+    alias: "e",
+    type: "boolean" as const,
+    demandOption: true,
+  },
+  specialFeatures: {
+    description: "Funko Special Features",
+    alias: "s",
+    type: "string" as const,
+    demandOption: true,
+  },
+  marketValue: {
+    description: "Funko Market Value",
+    alias: "m",
+    type: "number" as const,
+    demandOption: true,
+  },
+};
+
 yargs(hideBin(process.argv))
-  .command(
-    "add",
-    "Add a Funko",
-    {
-      username: {
-        description: "Username",
-        alias: "u",
-        type: "string",
-        demandOption: true,
-      },
-      id: {
-        description: "Funko ID",
-        alias: "i",
-        type: "string",
-        demandOption: true,
-      },
-      name: {
-        description: "Funko Name",
-        alias: "n",
-        type: "string",
-        demandOption: true,
-      },
-      description: {
-        description: "Funko Description",
-        alias: "d",
-        type: "string",
-        demandOption: true,
-      },
-      type: {
-        description: "Funko Type",
-        alias: "t",
-        type: "string",
-        choices: Object.values(funkoType),
-        demandOption: true,
-      },
-      genre: {
-        description: "Funko Genre",
-        alias: "g",
-        type: "string",
-        choices: Object.values(funkoGenre),
-        demandOption: true,
-      },
-      franchise: {
-        description: "Funko Franchise",
-        alias: "f",
-        type: "string",
-        demandOption: true,
-      },
-      number: {
-        description: "Funko Number",
-        alias: "num",
-        type: "number",
-        demandOption: true,
-      },
-      exclusive: {
-        description: "Funko Exclusive",
-        alias: "e",
-        type: "boolean",
-        demandOption: true,
-      },
-      specialFeatures: {
-        description: "Funko Special Features",
-        alias: "s",
-        type: "string",
-        demandOption: true,
-      },
-      marketValue: {
-        description: "Funko Market Value",
-        alias: "m",
-        type: "number",
-        demandOption: true,
-      },
-    },
-    (argv) => {
-      const service = new FunkoService(argv.username as string);
-      const funko = new Funko(
-        argv.id as string,
-        argv.name as string,
-        argv.description as string,
-        argv.type as funkoType,
-        argv.genre as funkoGenre,
-        argv.franchise as string,
-        argv.number as number,
-        argv.exclusive as boolean,
-        argv.specialFeatures as string,
-        argv.marketValue as number,
-      );
-      service.addFunko(funko);
-    },
-  )
-  .command(
-    "modify",
-    "Modify a Funko",
-    {
-      username: {
-        description: "Username",
-        alias: "u",
-        type: "string",
-        demandOption: true,
-      },
-      id: {
-        description: "Funko ID",
-        alias: "i",
-        type: "string",
-        demandOption: true,
-      },
-      name: {
-        description: "Funko Name",
-        alias: "n",
-        type: "string",
-        demandOption: true,
-      },
-      description: {
-        description: "Funko Description",
-        alias: "d",
-        type: "string",
-        demandOption: true,
-      },
-      type: {
-        description: "Funko Type",
-        alias: "t",
-        type: "string",
-        choices: Object.values(funkoType),
-        demandOption: true,
-      },
-      genre: {
-        description: "Funko Genre",
-        alias: "g",
-        type: "string",
-        choices: Object.values(funkoGenre),
-        demandOption: true,
-      },
-      franchise: {
-        description: "Funko Franchise",
-        alias: "f",
-        type: "string",
-        demandOption: true,
-      },
-      number: {
-        description: "Funko Number",
-        alias: "num",
-        type: "number",
-        demandOption: true,
-      },
-      exclusive: {
-        description: "Funko Exclusive",
-        alias: "e",
-        type: "boolean",
-        demandOption: true,
-      },
-      specialFeatures: {
-        description: "Funko Special Features",
-        alias: "s",
-        type: "string",
-        demandOption: true,
-      },
-      marketValue: {
-        description: "Funko Market Value",
-        alias: "m",
-        type: "number",
-        demandOption: true,
-      },
-    },
-    (argv) => {
-      const service = new FunkoService(argv.username as string);
-      const funko = new Funko(
-        argv.id as string,
-        argv.name as string,
-        argv.description as string,
-        argv.type as funkoType,
-        argv.genre as funkoGenre,
-        argv.franchise as string,
-        argv.number as number,
-        argv.exclusive as boolean,
-        argv.specialFeatures as string,
-        argv.marketValue as number,
-      );
-      service.modifyFunko(funko);
-    },
-  )
+  .command("add", "Add a Funko", commonFunkoOptions, (argv) => {
+    const service = new FunkoService(argv.username as string);
+    const funko = new Funko(
+      argv.id as string,
+      argv.name as string,
+      argv.description as string,
+      argv.type as funkoType,
+      argv.genre as funkoGenre,
+      argv.franchise as string,
+      argv.number as number,
+      argv.exclusive as boolean,
+      argv.specialFeatures as string,
+      argv.marketValue as number,
+    );
+    service.addFunko(funko);
+  })
+  .command("modify", "Modify a Funko", commonFunkoOptions, (argv) => {
+    const service = new FunkoService(argv.username as string);
+    const funko = new Funko(
+      argv.id as string,
+      argv.name as string,
+      argv.description as string,
+      argv.type as funkoType,
+      argv.genre as funkoGenre,
+      argv.franchise as string,
+      argv.number as number,
+      argv.exclusive as boolean,
+      argv.specialFeatures as string,
+      argv.marketValue as number,
+    );
+    service.modifyFunko(funko);
+  })
   .command(
     "remove",
     "Remove a Funko",
     {
-      username: {
-        description: "Username",
-        alias: "u",
-        type: "string",
-        demandOption: true,
-      },
-      id: {
-        description: "Funko ID",
-        alias: "i",
-        type: "string",
-        demandOption: true,
-      },
+      username: commonFunkoOptions.username,
+      id: commonFunkoOptions.id,
     },
     (argv) => {
       const service = new FunkoService(argv.username as string);
@@ -211,47 +124,23 @@ yargs(hideBin(process.argv))
   .command(
     "list",
     "List all Funkos",
-    {
-      username: {
-        description: "Username",
-        alias: "u",
-        type: "string",
-        demandOption: true,
-      },
-    },
+    { username: commonFunkoOptions.username },
     (argv) => {
       const service = new FunkoService(argv.username as string);
-      const funkos = service.listFunkos();
-      funkos.forEach((funko) => {
-        funko.printInfo();
-      });
+      service.listFunkos().forEach((funko) => funko.printInfo());
     },
   )
   .command(
     "show",
     "Show details of a Funko",
     {
-      username: {
-        description: "Username",
-        alias: "u",
-        type: "string",
-        demandOption: true,
-      },
-      id: {
-        description: "Funko ID",
-        alias: "i",
-        type: "string",
-        demandOption: true,
-      },
+      username: commonFunkoOptions.username,
+      id: commonFunkoOptions.id,
     },
     (argv) => {
       const service = new FunkoService(argv.username as string);
       const funko = service.getFunko(argv.id as string);
-      if (funko) {
-        funko.printInfo();
-      } else {
-        console.log("Funko not found");
-      }
+      funko ? funko.printInfo() : console.log("Funko not found");
     },
   )
   .demandCommand(1, "You need at least one command before moving on")
